f98e6ef4a5694fc5324386056e63c24f51393b62
cbhudson <cassio-hudson@uiowa.edu>
2015-12-02 00:07:44 -0600

    one more time...

--- db/messages.sql
+++ db/messages.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS messages;
 create table messages (
-	id bigint(20) NOT NULL,
+	id bigint(20) NOT NULL not null AUTO_INCREMENT,
 	title varchar(255) NULL,
 	message text null,
 	time_sent timestamp null,
 	is_read varchar(3) DEFAULT 'No',
 	read_at timestamp NULL,
-	sent_from int(11) not null,
+	is_from int(11) not null,
 	is_to int(11) not null,
 	service_id int not null,
 	primary key(id)
