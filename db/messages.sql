b78968818d849309914cfe5593a1798362dee2a0
cbhudson <cassio-hudson@uiowa.edu>
2015-12-01 23:59:18 -0600

    updated messages again, mysql man...

--- db/messages.sql
+++ db/messages.sql
@@ -4,8 +4,9 @@ create table messages (
 	title varchar(255) NULL,
 	message text null,
 	time_sent timestamp DEFAULT CURRENT_TIMESTAMP,
-	read varchar(3) default 'No',
-	from int(11) not null,
+	read varchar(3) DEFAULT 'No',
+	read_at timestamp NULL,
+	sent_from int(11) not null,
 	to int(11) not null,
 	service_id int not null,
 	primary key(id)
