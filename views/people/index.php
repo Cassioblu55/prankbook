663ecea69ee1a73149036b7233e39b78a0a52915
Hudson <cbhudson@serv42.divms.uiowa.edu>
2015-12-10 20:32:26 -0600

    Removed unneded files

--- views/people/index.php
+++
@@ -1,6 +0,0 @@
-<?php include_once '../templates/head.php';
-	include_once '../config/config.php';
-    // First we execute our common code to connection to the database and start the session 
-    require_once($serverPath."utils/common.php");
-?>
-<div>This will show a list of people.</div>
